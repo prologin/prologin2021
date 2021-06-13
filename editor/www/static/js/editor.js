d1de70d353b170fc16068c2c6eda6efbc2a57c62
Celian RAIMBAULT <celian.raimbault@epita.fr>
2021-06-13 11:40:42 +0100

    front/editor: Made texture loader

--- editor/www/static/js/editor.js
+++ editor/www/static/js/editor.js
@@ -1,10 +1,23 @@
+// TODO : Move to graphics
 // Init app
 let app = new PIXI.Application({width : VIEW_WIDTH, height : VIEW_HEIGHT});
 
 document.body.appendChild(app.view);
 
-const texture = PIXI.Texture.from('static/images/panda.png');
+// Load textures
+const TILES = [
+    'panda',
+    'panda_bebe',
+    'eau',
+];
 
-let sprite = new PIXI.Sprite(texture);
+let textures = {};
+
+for (let tile in TILES) {
+    textures[TILES[tile]] = PIXI.Texture.from(`static/images/${TILES[tile]}.png`);
+}
+
+// Draw
+let sprite = new PIXI.Sprite(textures['panda']);
 
 app.stage.addChild(sprite);
