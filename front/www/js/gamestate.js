1915322a05ea62db5f026158ea166082c2cb8ede
Nicolas Reyland <nicolas@reyland.net>
2021-06-15 11:41:26 +0200

    gamestate script (class skeleton)

---
+++ front/www/js/gamestate.js
@@ -0,0 +1,40 @@
+
+/*
+ - map
+ - pandas (positions)
+ - bebe pandas (positions)
+ - tiles
+*/
+
+class GameState {
+  // constructor
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.map = null;
+    this.pandas = null;
+    this.baby_pandas = null;
+    this.bridges = null;
+  }
+  // methods
+  function initMap() {
+    //
+  }
+  function initPandas() {
+    //
+  }
+  function initBabyPandas() {
+    //
+  }
+  function initBridges() {
+    //
+  }
+}
+
+
+
+
+
+
+
+//
