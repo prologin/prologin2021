9f04f4b40c9279a36e9017703f343ea2df2aae3a
Celian RAIMBAULT <celian.raimbault@epita.fr>
2021-06-20 19:12:01 +0100

    front/editor: Updated brush selector (all types of tile added)

--- editor/www/static/js/editor.js
+++ editor/www/static/js/editor.js
@@ -26,6 +26,10 @@ let brush = ['P', 1, 0, 0];
 let uiDumper = document.getElementById("dumper");
 let uiCanvas = document.getElementById("canvas");
 let uiBrush = document.getElementById("brush");
+let uiSelect1 = document.getElementById("select1");
+let uiSelect2 = document.getElementById("select2");
+
+onBrushChange();
 
 function onNewClick() {
     let dimension =
@@ -59,8 +63,94 @@ function onCopyClick() {
     document.execCommand("copy");
 }
 
+// When the brush selector has changed
 function onBrushChange() {
-    console.log(uiBrush.value);
+    // Update select1 / select2 extra options
+    switch (uiBrush.value) {
+        case "eau":
+            brush = null;
+            uiSelect1.hidden = true;
+            uiSelect2.hidden = true;
+            break;
+        case "panda":
+            brush = null;
+            uiSelect1.hidden = false;
+            uiSelect2.hidden = true;
+
+            onPandaBrush();
+            break;
+        case "pont":
+            brush = null;
+            uiSelect1.hidden = false;
+            uiSelect2.hidden = false;
+
+            onPontBrush();
+            break;
+    }
+}
+
+function onPandaBrush() {
+    // Clear node
+    while (uiSelect1.firstChild) {
+        uiSelect1.removeChild(uiSelect1.firstChild);
+    }
+
+    // Select1 = kind
+    let options = [
+        "panda1",
+        "panda2",
+        "panda1_bebe",
+        "panda2_bebe",
+    ]
+
+    for (let option of options) {
+        let node = document.createElement("option");
+        node.value = option;
+        node.text = option;
+
+        uiSelect1.appendChild(node);
+    }
+}
+
+function onPontBrush() {
+    // Clear nodes
+    while (uiSelect1.firstChild) {
+        uiSelect1.removeChild(uiSelect1.firstChild);
+    }
+
+    // Select1 = value
+    for (let i = 1; i <= 6; ++i) {
+        let option = `${i}`;
+
+        let node = document.createElement("option");
+        node.value = option;
+        node.text = option;
+
+        uiSelect1.appendChild(node);
+    }
+
+    // Clear nodes
+    while (uiSelect2.firstChild) {
+        uiSelect2.removeChild(uiSelect2.firstChild);
+    }
+
+    // Select2 = direction
+    let options = [
+        "n",
+        "ne",
+        "no",
+        "s",
+        "se",
+        "so",
+    ];
+
+    for (let option of options) {
+        let node = document.createElement("option");
+        node.value = option;
+        node.text = option;
+
+        uiSelect2.appendChild(node);
+    }
 }
 
 // --- Graphics ---
