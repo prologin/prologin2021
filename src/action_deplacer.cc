c93c7e219f34c2419f7bc3d1ded309fd71bda35b
Ramy Nassif <ramy.nassif@epita.fr>
2021-06-23 14:56:34 +0200

    rules/action_deplacer : Implemented actiondeplacer::check and actiondeplacer::apply_on, working on the tests

--- src/action_deplacer.cc
+++ src/action_deplacer.cc
@@ -5,11 +5,37 @@
 
 int ActionDeplacer::check(const GameState& st) const
 {
-    // FIXME
-    return 0;
+    const Panda* panda = st.player_at(player_id_)->panda_at(id_panda_);
+
+    // Maybe add some more conditions
+
+    if (panda == nullptr)
+        return ID_PANDA_INVALIDE;
+
+    position desired_position =
+        st.map().get_relative_position(panda->pos(), dir_);
+
+    if (st.map().is_valid(desired_position))
+        return DEPLACEMENT_HORS_LIMITES;
+
+    int valeur;
+    direction dir;
+    if (!st.map().get(desired_position).is_pont(&valeur, &dir) || dir != dir_)
+        return MOUVEMENT_INVALIDE;
+
+    return OK;
 }
 
 void ActionDeplacer::apply_on(GameState* st) const
 {
-    // FIXME
+    Map map = st->map();
+    Panda* panda = (Panda*)st->player_at(player_id_)->panda_at(id_panda_);
+    position desired_position = map.get_relative_position(panda->pos(), dir_);
+
+    // How do we want to manage errors???
+    map.set(desired_position, Cell::panda(player_id_, id_panda_));
+    map.set(panda->pos(),
+            Cell::empty()); // FIXME Should be replaces with pont...
+
+    panda->update_pos(desired_position);
 }
