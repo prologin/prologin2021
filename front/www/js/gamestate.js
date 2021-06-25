d62c22afc8613d358dfc133a06c2f7e93493d01d
Nicolas Reyland <nicolas@reyland.net>
2021-06-25 17:49:42 +0200

    redone gamestate reading from str & chmod +x on *.sh

--- front/www/js/gamestate.js
+++ front/www/js/gamestate.js
@@ -38,43 +38,35 @@ class GameState {
   initBridges(bridges) {
     this.bridges = bridges;
   }
-  loadGame(game_string) {
-    // format
-    let str = game_string.replace(/ /g, '');
-    str = str.replace(/\n/g, '');
-    console.log('str', str);
+  loadGame(map_str) {
     // reset/init
     this.initMap([[]]);
     this.initPandas([]);
     this.initBabyPandas([]);
     this.initBridges([]);
     // read map str
-    let x = 0, y = 0, obj = null;
-    for (var i = 0; i < str.length; i += 2) {
-      var c1 = str[i];
-      var c2 = str[i+1];
-      console.log('c12', c1, c2);
-      switch(c1) {
-        case '_': // water
-          obj = null;
-          break;
-        case 'P': // panda
-          obj = [c1, parseInt(c2), x, y]
-          this.pandas.push(obj); // use hex for id's ?
-          break;
-        case 'B': // baby panda
-          obj = [c1, parseInt(c2), x, y]
-          this.baby_pandas.push(obj);
-          break;
-        default: // bridge ?
-          obj = [c1, parseInt(c2), x, y];
-          this.bridges.push(obj);
-          break;
-      }
-      //
-      this.map[y][x] = obj;
+    var x = 0, y = 0, buffer = '';
+    for (let i = 0; i < map_str.length; i++) {
+      // get current char
+      let c = map_str[i];
 
-      // incr coordinates
+      // currently reading buffer ?
+      if (c != ' ' && c != '\n') {
+        // add char of tile to buffer and continue
+        buffer += c;
+        continue;
+      }
+
+      // - buffer has ended ! -
+
+      // add hte buffer to the map (pas sur ?)
+      this.map[y][x] = buffer; // j'avoue ne pas avoir bien compris qqchose, mais j'espère que cette ligne est OK ?
+
+      // process & reset buffer
+      this.processTileBuffer(buffer);
+      buffer = '';
+
+      // goto next coordinates
       x++;
       if (x == this.width) {
         this.x = 0;
@@ -82,21 +74,36 @@ class GameState {
       }
     }
   }
+  processTileBuffer(buffer) {
+    console.log("process: " + buffer + "\n");
+    // water tile
+    if (buffer == '__') {
+      return;
+    }
+    // panda tile
+    if (buffer[0] == 'P') {
+      let id = parseInt(buffer.substring(1));
+      this.pandas.push(id);
+    }
+    // bridge tile
+    if (buffer[0] == 'B') {
+      let id = parseInt(buffer.substring(1));
+      this.baby_pandas.push(id);
+    }
+    // baby panda
+    let id = parseInt(buffer);
+    this.baby_pandas.push(id);
+  }
 }
 
-/*
 function test() {
-  const s = '__ __ __ P1\n11 __ B2 __\n02 __ P2 __ __';
-  let gs = new GameState(4, 3);
-  gs.loadGame(s);
+  let test_map_str = '__ __ __ P1 __\n__ 11 __ B2 __\n02 __ P2 __ __';
+  let gs = new GameState(4, 4);
+  gs.loadGame(test_map_str);
   return gs;
 }
 
-let gs = test();
-*/
-
-
-
+// let gs = test();
 
 
 
