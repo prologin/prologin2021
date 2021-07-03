ac97c01058248658fa5c78b6aa55e11022ef449b
Celian RAIMBAULT <celian.raimbault@epita.fr>
2021-07-03 12:27:20 +0200

    front: Moved functions to share of replay to replay_utils, updated spectator main page

---
+++ front/www/js/replay_utils.js
@@ -0,0 +1,137 @@
+"use strict";
+
+// Some utilities used by the replay and the spectator
+
+// To use this script :
+// - Define the procedure fetchNextState()
+// - Define the predicate hasGameEnded()
+
+// --- Globals ---
+// The replay is a collection of game states
+let gameStates = [];
+
+// The current game state to render
+let currentGameStateIndex = 0;
+
+// Next state every N ms with this option
+let autoreplay = false;
+
+const AUTOREPLAY_PERIOD_MS = 500;
+
+function newGameState(width, height) {
+    // Update dimension values
+    mapWidth = width;
+    mapHeight = height;
+    updateViewSize();
+
+    // Update view dimensions
+    app.renderer.resize(mapWidth, mapHeight);
+
+    // Create new game state
+    gameState = new GameState(width, height);
+
+    updateView();
+
+    return gameState;
+}
+
+// --- UI ---
+let uiCanvas = document.getElementById("canvas");
+let uiStateIndicator = document.getElementById("state-indicator");
+let uiP1Points = document.getElementById("p1-points");
+let uiP2Points = document.getElementById("p2-points");
+let uiAutoReplay = document.getElementById("autoreplay");
+
+function onPrevClick() {
+    if (currentGameStateIndex > 0)
+        --currentGameStateIndex;
+
+    updateReplay();
+}
+
+function onNextClick() {
+    if (currentGameStateIndex < gameStates.length - 1) {
+        ++currentGameStateIndex;
+    } else {
+        // Try to add the next state (used by the spectator)
+        // If a state is added, call this function again
+        fetchNextState();
+
+        return;
+    }
+
+    // Render the next game state
+    updateReplay();
+}
+
+function onAutoReplayChange() {
+    autoreplay = !autoreplay;
+
+    if (autoreplay)
+        autoreplayLoop();
+    else
+        window.clearTimeout(autoreplayLoop);
+}
+
+// When a key is pressed
+function onKey(e) {
+    let useful = true;
+    switch (e.key.toUpperCase()) {
+    case " ":
+        // Toggle autoreplay
+        onAutoReplayChange();
+        uiAutoReplay.checked = autoreplay;
+        break;
+    case "N":
+        // Next state
+        onNextClick();
+        break;
+    case "B":
+        // Previous state
+        onPrevClick();
+        break;
+    default:
+        useful = false;
+        break;
+    }
+
+    if (useful)
+        e.preventDefault();
+}
+
+// --- Graphics ---
+updateViewSize();
+
+// Init
+initGraphics(uiCanvas, mapWidth, mapHeight, null);
+
+function updateReplay() {
+    if (gameStates.length == 0) return;
+
+    // gameState is the rendered state
+    gameState = gameStates[currentGameStateIndex];
+
+    // Update GUI
+    // TODO : Winner
+    uiStateIndicator.textContent =
+        hasGameEnded()
+            ? `Partie terminée (${gameStates.length} tours)`
+            : `${currentGameStateIndex + 1} / ${gameStates.length} tours`
+    // TODO : Display player points
+    uiP1Points.textContent = "Joueur 1 : ??? points";
+    uiP2Points.textContent = "Joueur 2 : ??? points";
+
+    updateView();
+}
+
+// Timer when we need to make an auto replay step
+function autoreplayLoop() {
+    autoreplayStep();
+
+    // Loop again
+    if (autoreplay)
+        window.setTimeout(autoreplayLoop, AUTOREPLAY_PERIOD_MS);
+}
+
+// Auto replay game state update
+function autoreplayStep() { onNextClick(); }
--- spectator/www/static/js/spectator.js
+++ spectator/www/static/js/spectator.js
@@ -2,20 +2,63 @@
 
 // Main spectator script
 
-function onStopClick() {
-    fetch("/action/stop");
-}
+let gameEnded = false;
 
-function onNextClick() {
-    // The server will play a turn and send the new game state
-    fetch("/action/next_state").then(res => {
-        if (res.status === 200) {
-            res.text().then(onNextStateFetched);
-        }
-    });
+// --- UI ---
+let uiGameInfo = document.getElementById("game-info");
+
+// --- Spectator ---
+function onStopClick() { fetch("/action/stop"); }
+
+function fetchNextState() {
+    if (!gameEnded) {
+        // The server will play a turn and send the new game state
+        fetch("/action/next_state").then(res => {
+            if (res.status === 200) {
+                res.text().then(onNextStateFetched);
+            }
+        });
+    }
 }
 
 function onNextStateFetched(nextStateJson) {
+    // If first state, update UI
+    if (uiCanvas.hidden) {
+        uiCanvas.hidden = false;
+        uiGameInfo.hidden = false;
+        document.getElementById("spectator-start").hidden = true;
+    }
+
     // TODO : Update state
     console.log(`Got next state ${nextStateJson}`);
+
+    // TODO : The nextStateJson will contain a field end of game
+    if (gameStates.length >= 10) {
+        endGame();
+        return;
+    }
+
+    // TODO : Remove (test)
+    let gs = newGameState(10, 10);
+    gs.panda_map[(currentGameStateIndex + 1) % 10][0].panda = new Panda(1, 1);
+    gameStates.push(gs);
+
+    // Update render
+    onNextClick();
 }
+
+function endGame() {
+    gameEnded = true;
+    updateReplay();
+}
+
+function hasGameEnded() {
+    return gameEnded && currentGameStateIndex == gameStates.length - 1;
+}
+
+// --- Main ---
+// -1 because there is no game state at all when we start the spectator
+currentGameStateIndex = -1;
+
+uiCanvas.hidden = true;
+uiGameInfo.hidden = true;
