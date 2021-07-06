3d2179e0dc4fc184238f00d144848f884d121d4a
Celian RAIMBAULT <celian.raimbault@epita.fr>
2021-07-06 18:59:03 +0200

    front/www: Fixed bug, graphics loaded two times

--- www/static/js/replay.js
+++ www/static/js/replay.js
@@ -8,9 +8,12 @@ $(function () {
     .done(function() {
         $.getScript('/static/js/replay_viewer.js')
         .done(function() {
-            start_replay($replay, $dump_content.text());
-            // reveal the UI
-            $replay.fadeIn('fast');
+            $.getScript('/static/js/replay_utils.js')
+            .done(function() {
+                start_replay($replay, $dump_content.text());
+                // reveal the UI
+                $replay.fadeIn('fast');
+            });
         });
     });
 
