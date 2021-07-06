1e84534ba760fd16031a3475f2e2754bd1648845
Celian RAIMBAULT <celian.raimbault@epita.fr>
2021-07-06 21:19:53 +0200

    front/www: The replay does not require dependencies anymore

--- www/static/js/replay.js
+++ www/static/js/replay.js
@@ -1,20 +1,36 @@
+function loadScripts(scripts, onLoaded) {
+    let scriptsLoaded = 0;
+
+    for (let script of scripts) {
+        $.getScript(script).done(function() {
+            ++scriptsLoaded;
+
+            if (scriptsLoaded === scripts.length) {
+                onLoaded();
+            }
+        })
+    }
+}
+
 $(function () {
     let $replay = $('#replay'),
         $dump_content = $('#dump');
 
     $replay.hide();
 
-    $.getScript('/static/js/pixi.min.js')
-    .done(function() {
-        $.getScript('/static/js/replay_viewer.js')
-        .done(function() {
-            $.getScript('/static/js/replay_utils.js')
-            .done(function() {
-                start_replay($replay, $dump_content.text());
-                // reveal the UI
-                $replay.fadeIn('fast');
-            });
-        });
-    });
+    let scripts = [
+        'static/js/pixi.min.js',
+        'static/js/prologin.js',
+        'static/js/graphics.js',
+        'static/js/gamestate.js',
+        'static/js/replay_utils.js',
+        'static/js/replay_viewer.js',
+    ];
 
+    loadScripts(scripts, function() {
+        start_replay($replay, $dump_content.text());
+
+        // reveal the UI
+        $replay.fadeIn('fast');
+    });
 });
