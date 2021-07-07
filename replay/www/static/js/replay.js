ebfe86fd3f14e80e77804e924b0b5c5065d56dd6
Celian RAIMBAULT <celian.raimbault@epita.fr>
2021-07-07 21:51:00 +0200

    front/replay: Parsed dump file

--- replay/www/static/js/replay.js
+++ replay/www/static/js/replay.js
@@ -4,57 +4,42 @@
 
 // --- Replay ---
 // Not used by the replay but by the spectator
-function fetchNextState() {
-}
+function fetchNextState() {}
 
 // Not used by the replay but by the spectator
-function hasGameEnded() {
-    return false;
-}
+function hasGameEnded() { return false; }
 
-// --- Test ---
-// // Create 3 states
-// for (let i = 0; i < 3; ++i)
-//     gameStates.push(newGameState(10, 10));
-
-// gameStates[0].panda_map[0][0].panda = new Panda(1, 1);
-// gameStates[1].panda_map[1][0].panda = new Panda(1, 1);
-// gameStates[2].panda_map[1][1].panda = new Panda(1, 1);
-
-// gameStates[0].panda_map[3][0].panda = new Panda(2, 1);
-// gameStates[1].panda_map[3][1].panda = new Panda(2, 1);
-// gameStates[2].panda_map[3][2].panda = new Panda(2, 1);
-
-// for (let gs of gameStates) {
-//     // TODO
-//     // for (let i = 0; i < 6; ++i) {
-//     //     for (let j = 0; j < 6; ++j) {
-//     //         gs.map[i][j].bridge = [i + 1, j + 1];
-//     //     }
-//     // }
-
-//     // gs.map[9][1].bridge = [5, 1];
-//     // gs.map[9][2].bridge = [3, 2];
-
-//     gs.panda_map[2][1].baby_panda = new BabyPanda(1, 1);
-//     gs.panda_map[2][2].baby_panda = new BabyPanda(2, 1);
-// }
-
+// --- Main ---
 // Load dump named dump.json if necessary
 fetch("/dump.json").then(res => {
-    if (res.status === 200)
+    if (res.status === 200) {
         res.text().then(loadDump);
-    else
-        console.error('No dump.json found');
+    } else {
+        // TODO : Remove test
+        console.error('No dump.json found, example dump parsed');
+
+        let dump = `{ "round": { "round_id": 0, "player_id": 0, "panda_id": 0 }, "map": { "size": { "x": 4, "y": 5 }, "cells": [ { "position": { "x": 0, "y": 0 }, "type": "PONT", "value": 1, "direction": "NORD_EST", "is_start": true, "panda": true, "player": 0, "id": 0 }, { "position": { "x": 0, "y": 1 }, "type": "PONT", "value": 4, "direction": "SUD_EST", "is_start": false, "panda": false }, { "position": { "x": 0, "y": 2 }, "type": "LIBRE" }, { "position": { "x": 0, "y": 3 }, "type": "PONT", "value": 6, "direction": "SUD_EST", "is_start": true, "panda": false }, { "position": { "x": 0, "y": 4 }, "type": "LIBRE" }, { "position": { "x": 1, "y": 0 }, "type": "PONT", "value": 3, "direction": "SUD_OUEST", "is_start": false, "panda": false }, { "position": { "x": 1, "y": 1 }, "type": "BEBE", "player": 0, "id": 0 }, { "position": { "x": 1, "y": 2 }, "type": "PONT", "value": 5, "direction": "NORD_OUEST", "is_start": true, "panda": true, "player": 0, "id": 1 }, { "position": { "x": 1, "y": 3 }, "type": "LIBRE" }, { "position": { "x": 1, "y": 4 }, "type": "PONT", "value": 2, "direction": "NORD_OUEST", "is_start": false, "panda": false }, { "position": { "x": 2, "y": 0 }, "type": "LIBRE" }, { "position": { "x": 2, "y": 1 }, "type": "LIBRE" }, { "position": { "x": 2, "y": 2 }, "type": "LIBRE" }, { "position": { "x": 2, "y": 3 }, "type": "PONT", "value": 1, "direction": "SUD", "is_start": true, "panda": true, "player": 1, "id": 0 }, { "position": { "x": 2, "y": 4 }, "type": "PONT", "value": 4, "direction": "NORD", "is_start": false, "panda": false }, { "position": { "x": 3, "y": 0 }, "type": "PONT", "value": 2, "direction": "SUD", "is_start": true, "panda": true, "player": 1, "id": 1 }, { "position": { "x": 3, "y": 1 }, "type": "PONT", "value": 6, "direction": "NORD", "is_start": false, "panda": false }, { "position": { "x": 3, "y": 2 }, "type": "LIBRE" }, { "position": { "x": 3, "y": 2 }, "type": "BEBE", "player": 1, "id": 0 }, { "position": { "x": 3, "y": 4 }, "type": "LIBRE" } ] }, "players": [ { "id": 0, "name": "anonymous", "score": 0, "total_babies": 1, "pandas": [ { "id": 0, "pos": { "x": 0, "y": 0 }, "saved_babies": 0 }, { "id": 1, "pos": { "x": 1, "y": 2 }, "saved_babies": 0 } ], "last_actions": [] }, { "id": 1, "name": "anonymous", "score": 0, "total_babies": 1, "pandas": [ { "id": 0, "pos": { "x": 2, "y": 3 }, "saved_babies": 0 }, { "id": 1, "pos": { "x": 3, "y": 0 }, "saved_babies": 0 } ], "last_actions": [] } ]}
+            { "round": { "round_id": 1, "player_id": 0, "panda_id": 0 }, "map": { "size": { "x": 4, "y": 5 }, "cells": [ { "position": { "x": 0, "y": 0 }, "type": "PONT", "value": 2, "direction": "NORD_EST", "is_start": true, "panda": true, "player": 0, "id": 0 }, { "position": { "x": 0, "y": 1 }, "type": "PONT", "value": 4, "direction": "SUD_EST", "is_start": false, "panda": false }, { "position": { "x": 0, "y": 2 }, "type": "LIBRE" }, { "position": { "x": 0, "y": 3 }, "type": "PONT", "value": 6, "direction": "SUD_EST", "is_start": true, "panda": false }, { "position": { "x": 0, "y": 4 }, "type": "LIBRE" }, { "position": { "x": 1, "y": 0 }, "type": "PONT", "value": 3, "direction": "SUD_OUEST", "is_start": false, "panda": false }, { "position": { "x": 1, "y": 1 }, "type": "BEBE", "player": 0, "id": 0 }, { "position": { "x": 1, "y": 2 }, "type": "PONT", "value": 5, "direction": "NORD_OUEST", "is_start": true, "panda": true, "player": 0, "id": 1 }, { "position": { "x": 1, "y": 3 }, "type": "LIBRE" }, { "position": { "x": 1, "y": 4 }, "type": "PONT", "value": 2, "direction": "NORD_OUEST", "is_start": false, "panda": false }, { "position": { "x": 2, "y": 0 }, "type": "LIBRE" }, { "position": { "x": 2, "y": 1 }, "type": "LIBRE" }, { "position": { "x": 2, "y": 2 }, "type": "LIBRE" }, { "position": { "x": 2, "y": 3 }, "type": "PONT", "value": 1, "direction": "SUD", "is_start": true, "panda": true, "player": 1, "id": 0 }, { "position": { "x": 2, "y": 4 }, "type": "PONT", "value": 4, "direction": "NORD", "is_start": false, "panda": false }, { "position": { "x": 3, "y": 0 }, "type": "PONT", "value": 2, "direction": "SUD", "is_start": true, "panda": true, "player": 1, "id": 1 }, { "position": { "x": 3, "y": 1 }, "type": "PONT", "value": 6, "direction": "NORD", "is_start": false, "panda": false }, { "position": { "x": 3, "y": 2 }, "type": "LIBRE" }, { "position": { "x": 3, "y": 3 }, "type": "BEBE", "player": 1, "id": 0 }, { "position": { "x": 3, "y": 4 }, "type": "LIBRE" } ] }, "players": [ { "id": 0, "name": "anonymous", "score": 0, "total_babies": 1, "pandas": [ { "id": 0, "pos": { "x": 0, "y": 0 }, "saved_babies": 0 }, { "id": 1, "pos": { "x": 1, "y": 2 }, "saved_babies": 0 } ], "last_actions": [] }, { "id": 1, "name": "anonymous", "score": 0, "total_babies": 1, "pandas": [ { "id": 0, "pos": { "x": 2, "y": 3 }, "saved_babies": 0 }, { "id": 1, "pos": { "x": 3, "y": 0 }, "saved_babies": 0 } ], "last_actions": [] } ]}`;
+        loadDump(dump);
+    }
 });
 
-
 // Called when there is a dump to load
+// The dump is multiple game states dumps separated by a new line
 function loadDump(data) {
-    gameStates.push(parseJSON(data));
+    let lines = data.split('\n');
 
-    console.log(gameStates);
+    if (lines.length === 0) {
+        console.error('Empty dump, cannot parse');
+        return;
+    }
 
+    // Parse
+    for (let line of lines) {
+        gameStates.push(parseJSON(line));
+    }
+
+    // Render
     mapWidth = gameStates[0].width;
     mapHeight = gameStates[0].height;
 
