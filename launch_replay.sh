579051093bf507190664943587c0fef55014c026
Celian RAIMBAULT <celian.raimbault@epita.fr>
2021-07-07 21:39:50 +0200

    front/replay: Added basic loadDump function (works with one game state)

--- launch_replay.sh
+++ launch_replay.sh
@@ -2,4 +2,11 @@
 
 # Starts the web server for the replay
 
+rm -f replay/www/dump.json
+
+if [ -n "$1" ]
+then
+    cp "$1" replay/www/dump.json
+fi
+
 python3 front_server.py replay
--- replay/www/static/js/replay.js
+++ replay/www/static/js/replay.js
@@ -13,34 +13,53 @@ function hasGameEnded() {
 }
 
 // --- Test ---
-// Create 3 states
-for (let i = 0; i < 3; ++i)
-    gameStates.push(newGameState(10, 10));
+// // Create 3 states
+// for (let i = 0; i < 3; ++i)
+//     gameStates.push(newGameState(10, 10));
 
-gameStates[0].panda_map[0][0].panda = new Panda(1, 1);
-gameStates[1].panda_map[1][0].panda = new Panda(1, 1);
-gameStates[2].panda_map[1][1].panda = new Panda(1, 1);
+// gameStates[0].panda_map[0][0].panda = new Panda(1, 1);
+// gameStates[1].panda_map[1][0].panda = new Panda(1, 1);
+// gameStates[2].panda_map[1][1].panda = new Panda(1, 1);
 
-gameStates[0].panda_map[3][0].panda = new Panda(2, 1);
-gameStates[1].panda_map[3][1].panda = new Panda(2, 1);
-gameStates[2].panda_map[3][2].panda = new Panda(2, 1);
+// gameStates[0].panda_map[3][0].panda = new Panda(2, 1);
+// gameStates[1].panda_map[3][1].panda = new Panda(2, 1);
+// gameStates[2].panda_map[3][2].panda = new Panda(2, 1);
 
-for (let gs of gameStates) {
-    // TODO
-    // for (let i = 0; i < 6; ++i) {
-    //     for (let j = 0; j < 6; ++j) {
-    //         gs.map[i][j].bridge = [i + 1, j + 1];
-    //     }
-    // }
+// for (let gs of gameStates) {
+//     // TODO
+//     // for (let i = 0; i < 6; ++i) {
+//     //     for (let j = 0; j < 6; ++j) {
+//     //         gs.map[i][j].bridge = [i + 1, j + 1];
+//     //     }
+//     // }
 
-    // gs.map[9][1].bridge = [5, 1];
-    // gs.map[9][2].bridge = [3, 2];
+//     // gs.map[9][1].bridge = [5, 1];
+//     // gs.map[9][2].bridge = [3, 2];
 
-    gs.panda_map[2][1].baby_panda = new BabyPanda(1, 1);
-    gs.panda_map[2][2].baby_panda = new BabyPanda(2, 1);
+//     gs.panda_map[2][1].baby_panda = new BabyPanda(1, 1);
+//     gs.panda_map[2][2].baby_panda = new BabyPanda(2, 1);
+// }
+
+// Load dump named dump.json if necessary
+fetch("/dump.json").then(res => {
+    if (res.status === 200)
+        res.text().then(loadDump);
+    else
+        console.error('No dump.json found');
+});
+
+
+// Called when there is a dump to load
+function loadDump(data) {
+    gameStates.push(parseJSON(data));
+
+    console.log(gameStates);
+
+    mapWidth = gameStates[0].width;
+    mapHeight = gameStates[0].height;
+
+    updateViewSize();
+    app.renderer.resize(mapWidth, mapHeight);
+    updateReplay();
+    startReplay();
 }
-
-// Render view
-updateReplay();
-
-startReplay();
