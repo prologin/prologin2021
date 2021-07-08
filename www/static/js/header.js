1c2817401223464045d3f390ed044cd04835af4f
Paul Guénézan <paul.guenezan@epita.fr>
2021-07-08 23:51:26 +0200

    www: add logo

---
+++ www/static/js/header.js
@@ -0,0 +1,26 @@
+
+let jqueryImport = document.createElement("script");
+jqueryImport.src = ""
+
+document.body.appendChild(jqueryImport);
+
+let scriptsUrl = [
+    '/static/js/jquery-3.6.0.min.js',
+    '/static/js/pixi.min.js',
+    '/static/js/prologin.js',
+    '/static/js/graphics.js',
+    '/static/js/gamestate.js',
+    '/static/js/viewer.js',
+];
+
+for (let script in scriptsUrl)
+{
+    let jqueryImport = document.createElement("script");
+    jqueryImport.src = script;
+    document.body.appendChild(jqueryImport);
+}
+
+$(function() {
+    $('[role=complementary] h1').text('').html('<object data="/static/img/logo_prologin_2021.png" style="width:100%; margin-top: 0.5em;" type="image/png" />');
+});
+
